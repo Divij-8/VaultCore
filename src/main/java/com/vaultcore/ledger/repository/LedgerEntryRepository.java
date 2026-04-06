44d8f6f5b0a6ddb1dc38e0b960a120abb22c89c6
Divij Mazumdar <divij.mazumdar@gmail.com>
2026-04-06 08:47:41 +0530

    add

--- src/main/java/com/vaultcore/ledger/repository/LedgerEntryRepository.java
+++ src/main/java/com/vaultcore/ledger/repository/LedgerEntryRepository.java
@@ -1,7 +1,19 @@
 package com.vaultcore.ledger.repository;
+
 import com.vaultcore.ledger.domain.LedgerEntry;
+import com.vaultcore.ledger.domain.LedgerEntryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, UUID> {
+
+    @Query("""
+        SELECT COALESCE(SUM(le.amount), 0)
+        FROM LedgerEntry le
+        WHERE le.account.id = :accountId AND le.entryType = :type
+    """)
+    BigDecimal sumAmountByAccountIdAndEntryType(UUID accountId, LedgerEntryType type);
 }
